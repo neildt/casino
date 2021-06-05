8e78fb505b8519c048ff61a4df8f90a4e149a9a4
Neil du Toit <neil@snail-mail.co.za>
2021-06-05 12:33:25 +0200

    Initial commit

---
+++ dagacube/src/main/java/uk/co/rank/casino/dagacube/DagacubeApplication.java
@@ -0,0 +1,13 @@
+package uk.co.rank.casino.dagacube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DagacubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DagacubeApplication.class, args);
+	}
+
+}
---
+++ dagacube/src/test/java/uk/co/rank/casino/dagacube/DagacubeApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.rank.casino.dagacube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DagacubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
